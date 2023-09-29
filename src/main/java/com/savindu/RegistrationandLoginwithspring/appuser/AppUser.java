957cffd52041b60b3e19039f5a07c5528cb36dc1
savindu29 <1savindupanagoda@gmail.com>
2023-09-29 16:34:39 +0530

    created AppUser

--- src/main/java/com/savindu/RegistrationandLoginwithspring/appuser/AppUser.java
+++ src/main/java/com/savindu/RegistrationandLoginwithspring/appuser/AppUser.java
@@ -1,22 +1,49 @@
 package com.savindu.RegistrationandLoginwithspring.appuser;
 
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
-
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
 public class AppUser implements UserDetails {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String username;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private Boolean locked;
     private boolean enabled;
 
+
+    public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
