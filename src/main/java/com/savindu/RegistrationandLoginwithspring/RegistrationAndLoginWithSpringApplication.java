c11fe5140fd82321f520e95d471d315ff23396bc
savindu29 <1savindupanagoda@gmail.com>
2023-09-29 11:09:26 +0530

    first commit

---
+++ src/main/java/com/savindu/RegistrationandLoginwithspring/RegistrationAndLoginWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.savindu.RegistrationandLoginwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAndLoginWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationAndLoginWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savindu/RegistrationandLoginwithspring/RegistrationAndLoginWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savindu.RegistrationandLoginwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
